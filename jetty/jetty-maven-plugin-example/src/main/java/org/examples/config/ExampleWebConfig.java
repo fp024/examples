d39563fbb42e9dcbc2ea91133cf1c90e4bbf4b5b
fp024 <aopenasus@naver.com>
2024-08-18 07:52:21 +0900

    favicon.ico 처리 수정

--- jetty/jetty-maven-plugin-example/src/main/java/org/examples/config/ExampleWebConfig.java
+++ jetty/jetty-maven-plugin-example/src/main/java/org/examples/config/ExampleWebConfig.java
@@ -3,6 +3,7 @@ package org.examples.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +14,12 @@ public class ExampleWebConfig implements WebMvcConfigurer {
   @Override
   public void configureViewResolvers(ViewResolverRegistry registry) {
     registry.jsp("/WEB-INF/views/", ".jsp");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/resources/**", "/favicon.ico")
+        .addResourceLocations("/resources/");
   }
 }
